6cdbfa7a1b4e2f2e6466c52c27352c3f01bc27d4
Yuriy Perevoznikov <yuriless@gmail.com>
2015-10-24 21:50:42 +0300

    added navigation controller to angular app

---
+++ assets/js/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name canvasAppApp.controller:NavbarcontrollerCtrl
+ * @description
+ * # NavbarcontrollerCtrl
+ * Controller of the canvasAppApp
+ */
+angular.module('canvasApp')
+  .controller('NavbarCtrl', function ($scope) {
+    console.log('test!!!');
+  });
